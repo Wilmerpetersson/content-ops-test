646bc52602ea4d9efae9859fac8c9cb900d1ea5b
Wilmer Petersson <wilmer@MBPsomtorWilmer.lan>
2024-08-26 01:33:31 +0200

    added honeypot to form

--- src/components/blocks/FormBlock/index.tsx
+++ src/components/blocks/FormBlock/index.tsx
@@ -69,7 +69,10 @@ export default function FormBlock(props) {
             onSubmit={handleSubmit}
             ref={formRef}
             data-sb-field-path={fieldPath}
+            method="POST"
+            netlify-honeypot="bot-field"
             data-netlify="true"
+
         >
             <div
                 className={classNames('w-full', 'flex', 'flex-wrap', 'gap-8', mapStyles({ justifyContent: styles?.self?.justifyContent ?? 'flex-start' }))}
